c7bb77308a5183cc52e8bbee0412f62b3729dfec
Jamie Norman <jamienorman@me.com>
2016-04-19 16:40:26 +0000

    test code for redis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,7 +37,7 @@ Route::get('test', function () {
 
 Route::get('check', function () {
 
-
+    Redis::publish('test-channel', json_encode(['foo' => 'bar']));
     $logFiles = array('CAM','LoadTest','Fusion','SAM');
 
 
