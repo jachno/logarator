757f65c461a0d55de96a5952b85a4ac9faff810e
Jamie Norman <jamienorman@me.com>
2016-04-20 20:27:50 +0000

    change key

--- config/app.php
+++ config/app.php
@@ -93,7 +93,7 @@ return [
 
     'key' => env('APP_KEY'),
 
-    'cipher' => 'AES-256-CBC',
+    'cipher' => '2/AYc2t30O5EauDRPL2WmhpvOTvS0v32oFk04Fb1c7c=',
 
     /*
     |--------------------------------------------------------------------------
